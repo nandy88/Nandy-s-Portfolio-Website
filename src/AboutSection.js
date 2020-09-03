07f5cce7739614aa76fb732424ce1d7f31faa6d1
Nandy Rodrigo <nandy.rodrigo@gmail.com>
2020-09-04 07:49:15 +1000

    lorem ipsum added

--- src/AboutSection.js
+++ src/AboutSection.js
@@ -4,7 +4,7 @@ import './AboutSection.css'
 export default class AboutSection extends React.Component{
 
   state = {
-    content: "I am Batman!!!",
+    content: "Morbi sed est vel tortor volutpat porta vitae eu metus. Sed molestie felis efficitur augue aliquam sagittis. Nullam mollis vulputate nisl, sit amet semper quam. Aenean ultricies nulla a mi tempus, nec feugiat urna elementum. Duis sed justo felis. Quisque turpis elit, tristique id tellus sit amet, euismod ornare lectus. Integer maximus dolor sit amet diam pretium, imperdiet lacinia elit ultricies. Aenean congue ligula vel ex iaculis, id consectetur tortor mattis. Nunc ac mauris neque. Nulla bibendum semper laoreet. Praesent non nibh vel ante laoreet porta vitae eget arcu. Morbi eget feugiat magna. Praesent eget elit sem.",
     firstListItem: "first-about-list-item",
     secondListItem: "about-list-item-no-underline",
     thirdListItem: "about-list-item-no-underline",
@@ -13,7 +13,7 @@ export default class AboutSection extends React.Component{
 
   whoAmI = () => {
     this.setState({
-      content: "I am Batman!!!",
+      content: "Morbi sed est vel tortor volutpat porta vitae eu metus. Sed molestie felis efficitur augue aliquam sagittis. Nullam mollis vulputate nisl, sit amet semper quam. Aenean ultricies nulla a mi tempus, nec feugiat urna elementum. Duis sed justo felis. Quisque turpis elit, tristique id tellus sit amet, euismod ornare lectus. Integer maximus dolor sit amet diam pretium, imperdiet lacinia elit ultricies. Aenean congue ligula vel ex iaculis, id consectetur tortor mattis. Nunc ac mauris neque. Nulla bibendum semper laoreet. Praesent non nibh vel ante laoreet porta vitae eget arcu. Morbi eget feugiat magna. Praesent eget elit sem.",
       firstListItem: "first-about-list-item",
       secondListItem: "about-list-item-no-underline",
       thirdListItem: "about-list-item-no-underline",
@@ -24,7 +24,7 @@ export default class AboutSection extends React.Component{
 
   myStory = () => {
     this.setState({
-      content: "I am Superman!!!",
+      content: "Aliquam luctus feugiat porttitor. Vestibulum urna sem, efficitur ac viverra ac, ornare et nibh. Pellentesque porta neque sem, et cursus magna hendrerit a. Aliquam lacinia condimentum felis quis suscipit. Nulla sit amet mauris vel nulla cursus mattis ut ut augue. Sed tincidunt massa ut ipsum dignissim eleifend. Fusce vitae justo ipsum. Suspendisse sagittis eleifend ullamcorper. Fusce luctus tellus at arcu volutpat malesuada.",
       firstListItem: "first-about-list-item-no-underline",
       secondListItem: "about-list-item",
       thirdListItem: "about-list-item-no-underline",
@@ -34,7 +34,7 @@ export default class AboutSection extends React.Component{
 
   myInterests = () => {
     this.setState({
-      content: "I am Green Lantern!!!",
+      content: "Ut quis feugiat lorem. Quisque vestibulum eget nibh eu aliquam. Aenean quis aliquam lorem, in tempus velit. In tellus neque, interdum sit amet felis fermentum, tempus imperdiet lectus. Vivamus posuere nisi eu accumsan aliquet. Fusce gravida elit ac sagittis egestas. Praesent luctus ornare commodo. Mauris ut mauris tellus.",
       firstListItem: "first-about-list-item-no-underline",
       secondListItem: "about-list-item-no-underline",
       thirdListItem: "about-list-item",
@@ -44,7 +44,7 @@ export default class AboutSection extends React.Component{
 
   myAspirations = () => {
     this.setState({
-      content: "I am Flash!!!",
+      content: "Proin faucibus ut turpis et dictum. Suspendisse nec nisi sed sapien viverra luctus eget eu magna. Sed sed nulla augue. Nullam a porttitor odio. Pellentesque accumsan diam nibh. Cras sit amet dolor non justo dignissim hendrerit ac eget justo. Nulla facilisi. Praesent at sapien semper justo commodo euismod id et ante. Cras enim mauris, imperdiet sit amet mattis ac, accumsan eu lectus. Donec est ex, interdum non euismod nec, fermentum id ligula.",
       firstListItem: "first-about-list-item-no-underline",
       secondListItem: "about-list-item-no-underline",
       thirdListItem: "about-list-item-no-underline",
--- src/ContactSection.js
+++ src/ContactSection.js
@@ -15,27 +15,30 @@ export default class ContactSection extends React.Component{
     return(
       <div className="contact-wrapper">
         <div className="resume-wrapper">
+          <p className="contact-text">Quisque hendrerit, augue a aliquet dapibus, leo risus placerat lorem, eu porta magna orci eget est. Fusce dignissim ornare ante vel tincidunt. Sed commodo leo id sem aliquet interdum. Donec malesuada ullamcorper orci nec auctor. Phasellus consequat ligula in leo vehicula varius. Ut eget eros dolor. Nunc aliquet tempus nisl, sit amet pretium nunc dapibus eu. Suspendisse ac neque vitae quam pretium sollicitudin sagittis non leo. Vestibulum laoreet molestie nibh, quis vestibulum nunc laoreet eget. Integer eget enim imperdiet orci venenatis aliquam. Donec semper justo nec tellus elementum iaculis. Suspendisse lacinia, justo ut pulvinar rutrum, elit lorem facilisis sem, vel egestas sapien justo vitae felis. Nullam ultricies erat non purus iaculis dapibus.</p>
+          <br/>
           <span className="download-resume-link">
             <a href={ResumePdf} target="_blank">DOWLOAD RESUME</a>
           </span>
-          <p></p>
         </div>
-        <div className="mobile">
-        <span><FontAwesomeIcon href="" icon={faMobileAlt} size="2x"/></span>
-          <p><span>Mobile:</span> 0430 045 676</p>
-        </div>
-        <div className="email">
-          <span><FontAwesomeIcon href="" icon={faEnvelope} size="2x"/></span>
-          <p><span>Email:</span> nandy.t.rodrigo@gmail.com</p>
-        </div>
-        <div className="github">
-          <a href="https://github.com/nandy88" target="_blank"><FontAwesomeIcon href="" icon={faGithub} size="2x"/></a>
-          <p><span>GitHub:</span> https://github.com/nandy88</p>
-        </div>
-        <div className="linkedin">
-          <a href="https://au.linkedin.com/in/nandy-rodrigo" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
-          <p><span>LinkedIn:</span> https://au.linkedin.com/in/nandy-rodrigo</p>
-        </div>
+        <section className="contact-fields">
+          <div className="mobile">
+          <span><FontAwesomeIcon href="" icon={faMobileAlt} size="2x"/></span>
+            <p><span>Mobile:</span> 0430 045 676</p>
+          </div>
+          <div className="email">
+            <span><FontAwesomeIcon href="" icon={faEnvelope} size="2x"/></span>
+            <p><span>Email:</span> nandy.t.rodrigo@gmail.com</p>
+          </div>
+          <div className="github">
+            <a href="https://github.com/nandy88" target="_blank"><FontAwesomeIcon href="" icon={faGithub} size="2x"/></a>
+            <p><span>GitHub:</span> https://github.com/nandy88</p>
+          </div>
+          <div className="linkedin">
+            <a href="https://au.linkedin.com/in/nandy-rodrigo" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
+            <p><span>LinkedIn:</span> https://au.linkedin.com/in/nandy-rodrigo</p>
+          </div>
+        </section>
       </div>
     )
   }
